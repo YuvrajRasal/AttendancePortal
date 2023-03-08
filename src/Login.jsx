c037e13a78962923b1ad30227e4ffa440cef9a55
YuvrajRasal <yuvraj8918@gmail.com>
2023-03-08 22:57:46 +0530

    new changes chng: display flex & padding in paper

--- src/Login.jsx
+++ src/Login.jsx
@@ -22,7 +22,7 @@ import "./Login.css";
 //
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 const paperStyle = {
-  display: "flex",
+  // display: "flex",
   alignItems: "center",
   justifyContent: "center",
   marginTop: "15px",
@@ -102,7 +102,7 @@ function Login() {
                 {/* ----inserted padding */}
                 <Box className="box">
                   <img
-                  className="libraryImage"
+                  className="logoImage"
                     src={logo}
                     style={{
                       width: "32%",
